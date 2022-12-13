98ce0aee39e922fa50659a82aab0770e1618c902
user <gdpphamthanh@gmail.com>
2022-12-13 14:56:36 +0700

    category_service

--- category.py
+++ category.py
@@ -60,7 +60,7 @@ def all_category(page: int, limit: int, response: Response):
         offset = (page - 1) * limit
         if page > total_page or page <= 0:
             response.status_code = status.HTTP_400_BAD_REQUEST
-            return f"page is not exist, total page is {total_page}"
+            return f"no record found"
         my_cursor.execute("SELECT * FROM categories ORDER BY id ASC LIMIT %s OFFSET %s", (limit, offset))
         categories = my_cursor.fetchall()
         if categories is None:
